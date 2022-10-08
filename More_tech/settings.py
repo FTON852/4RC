c673179446fc2d0e7728b3aba924ce0171adb4a1
Vladimir <justlikevova@gmail.com>
2022-10-08 14:43:18 +0300

    Gamification
    add models

--- More_tech/settings.py
+++ More_tech/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'main',
     'marketplace',
     'eventfeed',
+    'gamification',
 ]
 
 MIDDLEWARE = [
---
+++ gamification/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from main.models import Account
+
+
+class Achievement(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=2000)
+    reward = models.FloatField()
+
+
+class Achievements(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    achieve = models.ForeignKey(Achievement, on_delete=models.CASCADE)
