3234ddbe647e78db76f6bf769cce3b5c2b3440ee
Vladimir <justlikevova@gmail.com>
2022-10-08 20:07:15 +0300

    Revert "Merge branch 'back-end' of https://github.com/FTON852/4RC into back-end"
    This reverts commit 93eadd6089f2b1dc13ac8838eae7763be631e3c5, reversing
    changes made to 03a6e4e861756db9e93b4f14be68cb2e61ac5142.

--- main/views.py
+++ main/views.py
@@ -9,17 +9,7 @@ from .forms import TransferForm
 @login_required
 def index(request):
     user = Account.objects.get(user_id=request.user.id)
-    return render(request, 'index.html',
-                  {
-                      'user': user,
-                  })
-
-
-@login_required
-def profile(request, pk):
-    resiever = Account.objects.get(pk=pk)
-    sender = Account.objects.get(user_id=request.user.id)
-    core = Core(sender.wallet.public_key, sender.wallet.secret_key)
+    core = Core(user.wallet.public_key, user.wallet.secret_key)
     balance = core.balance
     balance_nft = core.balance_nft
     nfts = []
@@ -28,24 +18,35 @@ def profile(request, pk):
             "amount": len(nft["tokens"]),
             "uri": nft["uri"]
         })
+    return render(request, 'index.html',
+                  {
+                      'user': user,
+                      'matic': balance["maticAmount"],
+                      'ruble': balance["coinsAmount"],
+                      "balance_nft": nfts
+                  })
+
+
+@login_required
+def profile(request, pk):
+    resiever = Account.objects.get(pk=pk)
+    sender = Account.objects.get(user_id=request.user.id)
+    
     if request.method == 'POST':
         form = TransferForm(request.POST)
         if form.is_valid():
             user_money_count = 123  # from api sender
-            money_count = int(form.cleaned_data['money_count'])
-            if money_count > user_money_count:
+            money_count = form.cleaned_data['money_count']
+            if int(money_count) > user_money_count:
                 print(f"You don't have {money_count} money")
             else:
-                core.send_ruble(resiever.wallet.public_key, money_count)
+                # транзакция с переводом снять у отправителя, положить получателю
+                pass
+        return render(request, 'profile.html', {'user': resiever, 'form': form})
+
     else:
         form = TransferForm()
-
-    return render(request, 'profile.html', {'user': resiever,
-                                            'form': form,
-                                            'matic': balance["maticAmount"],
-                                            'ruble': balance["coinsAmount"],
-                                            'balance_nft': nfts,
-                                            })
+    return render(request, 'profile.html', {'user': resiever, 'form': form})
 
 
 @login_required
