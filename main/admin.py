c9117d791d1d7368fc8686f6be36c2b876514794
Vladimir <justlikevova@gmail.com>
2022-10-08 14:43:54 +0300

    main acc, wallet
    add user account and wallet model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Account, Wallet
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Wallet)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Wallet(models.Model):
+    secret_key = models.CharField(max_length=100)
+    public_key = models.CharField(max_length=100)
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    icon = models.ImageField(blank=True, default='icon.jpg')
+    status = models.BooleanField()
+    wallet = models.ForeignKey(Wallet, related_name='wallet', on_delete=models.CASCADE)
