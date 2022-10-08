2c3a8f61767bdd7c59581d9588be64f005795739
DimaRFF <dimarff.pro@gmail.com>
2022-10-08 18:01:46 +0300

    send money to person

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Account
 from api.network import Core
+from .forms import TransferForm
 
 
 # Create your views here.
@@ -28,8 +29,24 @@ def index(request):
 
 @login_required
 def profile(request, pk):
-    user = Account.objects.get(pk=pk)
-    return render(request, 'profile.html', {'user': user})
+    resiever = Account.objects.get(pk=pk)
+    sender = Account.objects.get(user_id=request.user.id)
+    
+    if request.method == 'POST':
+        form = TransferForm(request.POST)
+        if form.is_valid():
+            user_money_count = 123  # from api sender
+            money_count = form.cleaned_data['money_count']
+            if int(money_count) > user_money_count:
+                print(f"You don't have {money_count} money")
+            else:
+                # транзакция с переводом снять у отправителя, положить получателю
+                pass
+        return render(request, 'profile.html', {'user': resiever, 'form': form})
+
+    else:
+        form = TransferForm()
+    return render(request, 'profile.html', {'user': resiever, 'form': form})
 
 
 @login_required
