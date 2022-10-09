54cd82a5e58909eb0216044f743feac3347bf0c4
Vladimir <justlikevova@gmail.com>
2022-10-09 03:48:43 +0300

    login

--- main/models.py
+++ main/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 from django.urls import reverse
 
@@ -39,6 +43,20 @@ class Account(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+# autocreate account from user
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        account = Account.objects.create(user=instance)
+        account.create_wallet()
+        account.save()
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.account.save()
 
 
 class MonthlyPay(models.Model):
